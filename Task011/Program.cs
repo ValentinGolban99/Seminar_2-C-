c37f5141d2c3bdf1d71d67cc5647c07dc6dc8e9a
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-08-24 17:10:34 +0300

    task011

---
+++ Task011/Program.cs
@@ -0,0 +1,12 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+string number = Convert.ToString (new Random().Next(100, 1000));
+//Convert.ToString (number) - переводит число в формат "строка"
+Console.WriteLine (number);
+Console.WriteLine (number[0] + "" + number[2]);
+
+
+
